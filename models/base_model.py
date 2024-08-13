cc420d625df642a5722a619b514e0c0d7698de0f
kenyancoder254 <kenyancoder05@gmail.com>
2024-08-13 15:56:46 +0300

    Create the base class

--- models/base_model.py
+++ models/base_model.py
@@ -6,13 +6,21 @@ from datetime import datetime
 
 class BaseModel:
     """Base class for the project"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         initializes public attributes with values
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = self.created_at
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now().isoformat()
+            self.updated_at = self.created_at
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated\
+                    _at'], '%Y-%m-%d%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created\
+                    _at'], '%Y-%m-%d%H:%M:%s.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """string representation of the class"""
