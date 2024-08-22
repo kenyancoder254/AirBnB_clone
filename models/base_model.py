27679c2c7021680f72a791b62dc3e8c53d0027df
kenyancoder254 <kenyancoder05@gmail.com>
2024-08-22 23:10:19 +0300

    Enable the base model to take variable no. of arguments

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,21 @@ from datetime import datetime
 class BaseModel:
     """Base class of the project"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """creates public instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """String rep of the class"""
